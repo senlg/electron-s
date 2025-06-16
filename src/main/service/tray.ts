e7ba2cbd80ba90b131b6906c17cb6fbd6f055778
swl <929040862@qq.com>
2025-06-16 10:21:17 +0800

    up

--- src/main/service/tray.ts
+++ src/main/service/tray.ts
@@ -1,12 +1,17 @@
 import { GlobalObj } from '@main/global'
-import { Menu, MenuItem, nativeImage, Tray } from 'electron'
-import logo from '@/../resources/icons/icon.ico'
-import icon2 from '@/../resources/icons/icon.png'
+import { app, Menu, MenuItem, nativeImage, Tray } from 'electron'
+// import logo from '@/../resources/icons/icon.ico?raw'
+// import icon2 from '@/../resources/icons/icon.png?raw'
 import { config } from '@main/global/config'
+import path from 'path'
 
 export const initTray = () => {
-  let icon = nativeImage.createFromPath(logo)
-  let i = nativeImage.createFromPath(icon2).resize({ width: 16, height: 16 })
+  let icon = nativeImage.createFromPath(path.resolve(__dirname, '../../resources/icons/icon.ico'))
+  console.log('path icon', path.resolve(__dirname, '../../resources/icons/icon.png'))
+
+  let i = nativeImage
+    .createFromPath(path.resolve(__dirname, '../../resources/icons/icon.png'))
+    .resize({ width: 16, height: 16 })
   GlobalObj.tray = new Tray(icon)
   // 设置托盘
   setTray(GlobalObj.tray)
@@ -14,7 +19,10 @@ export const initTray = () => {
   let menu: (MenuItem | Electron.MenuItemConstructorOptions)[] = [
     {
       icon: i,
-      label: '测试'
+      label: '退出',
+      click: () => {
+        app.quit()
+      }
     }
   ]
   const contextMenu = Menu.buildFromTemplate(menu)
